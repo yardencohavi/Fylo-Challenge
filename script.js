d6486ff78d56271120557c64b82bc4268b8c83a4
yarden cohavi <cohaviyarden@gmail.com>
2021-01-16 18:07:39 +0200

    js

---
+++ script.js
@@ -0,0 +1,42 @@
+var leftStorage = 50,
+    usedStorage = 0;
+var precentage = 0;
+
+var domLeft = document.getElementById("left");
+var domUsed = document.getElementById("use");
+var domBar = document.getElementById("bar");
+
+var inputFile = document.getElementById("file");
+var fileList = {};
+var sizeFile = 0,
+    nameFile;
+
+
+inputFile.onchange = function () {
+
+    sizeFile = (this.files[0].size / 1024 / 1024); //size in MB
+    nameFile = this.files[0].name;
+
+    if (sizeFile > 1.5) {
+        alert("File is too big!");
+        this.value = "";
+    } else {
+        if (leftStorage > sizeFile) {
+            fileList[nameFile] = sizeFile.toFixed(1);
+            usedStorage += sizeFile;
+            leftStorage -= sizeFile;
+            precentage += ((sizeFile / 50) * 100);
+        } else
+            alert("There is no enough space!");
+    };
+
+    domLeft.textContent = leftStorage.toFixed(1);
+    domUsed.textContent = usedStorage.toFixed(1);
+    domBar.style.width = precentage.toFixed(1) + "%";
+
+    var rows = "";
+    rows += "<tr><td>" + nameFile + "</td><td>" + sizeFile.toFixed(1) +"</td></tr>";
+    $(rows).appendTo("#list tbody");
+};
+
+
